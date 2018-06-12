25682cc30d01959ec81028fbfb38305cf3a5c6ad
Mike Millar <39285355+MikeMillar@users.noreply.github.com>
2018-06-11 23:34:28 -0600

    add html link, add css style, create dropdown
    added links to on-page ancors in html, modified some classes/id's. added css to work with javascript for the working dropdown menu.

--- resources/js/main.js
+++ resources/js/main.js
@@ -8,7 +8,23 @@ $(document).ready(function(){
   //  }
   //});
 
-  $("#nav-toggle").on("click", function() {
-    $(".mobile-list").toggle();
+
+//jQuery JS used to show/hide mobile navigation
+  $("#nav-toggle").on("click", function(event) {
+    event.preventDefault();
+    $("mobile-list").css("display", "none");
+    $(".mobile-list").slideToggle();
+    $("i", this).toggleClass("fa-bars fa-times")
   });
+
+  $('.mobile-list a').on("click", function(event) {
+    event.preventDefault();
+    var headerHeight = $("header").height();
+    var id = $(this).attr("href");
+    var originalScrollCordinate = $(id).offset().top;
+    var newScrollCordinate = originalScrollCordinate - headerHeight;
+    $("html").animate({
+      scrollTop: newScrollCordinate
+    });
+  })
 })
